e5d245fd97dc18069bb21f2d57c6fbb37f12896b
austin smythe <aust.burner@gmail.com>
2020-02-12 17:37:18 -0700

    Initial commit

---
+++ Manual-exponent.js
@@ -0,0 +1,11 @@
+// toThePowerOf(2, 3) => 8
+// reduce
+
+// const toThePowerof = (num, exp) => {
+//     const items = Array(exp).fill(num)
+// }
+
+// Array(3).fill(2); //? [2, 2, 2]
+
+let s = math.pow(3, 3)
+console.log(s)
